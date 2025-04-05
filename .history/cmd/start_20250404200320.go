6920ebccf4ffecf77a9c725f0fce7dd3777b013a
jackli <jacknotes@163.com>
2025-04-05 18:46:06 +0800

    update

--- .history/cmd/start_20250404200320.go
+++
@@ -1,8 +0,0 @@
-
-
-
-
-func init() {
-	StartCmd.PersistentFlags().StringVarP(&confFile, "config", "f", "codelist.txt", "code list文件路径")
-	RootCmd.AddCommand(StartCmd)
-}
--- .history/main_20250404195915.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Println(err)
-	}
-}
